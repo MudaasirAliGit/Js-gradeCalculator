dab2c12c0eca63d8b75ca3a62873c1f452f8b353
MudaasirAliGit <mudassirali101704@gmail.com>
2024-09-12 14:17:13 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// Grade Calculator
+// Prepared by Mudassir Ali
+
+let grade = Number(prompt("Please enter the student score: "));
+
+if(grade >= 90){
+    alert("The student's grade is A.");
+}
+else if(grade >= 80){
+    alert("The student's grade is B.");
+}
+else if(grade >= 70){
+    alert("The student's grade is C.");
+}
+else if(grade >= 60){
+    alert("The student's grade is D.");
+}
+else{
+    alert("The student's grade is F.");
+}
+
+
+
